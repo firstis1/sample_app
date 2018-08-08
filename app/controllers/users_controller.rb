d1e757969c2f94d7647d5d00f5a8de5eca46910c
User <testuser@example.com>
2018-08-08 11:41:51 +0700

    Chapter 12 Password reset

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,76 @@
+class UsersController < ApplicationController
+  before_action :load_user, only: :show
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
+  def show; end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      @user.send_activation_email
+      flash[:info] = t("please_check_email_active")
+      redirect_to root_url
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = t("profile_updated")
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = t("user_deleted", id: params[:id])
+    redirect_to users_url
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+      :password_confirmation)
+  end
+
+  def load_user
+    if @user = User.find_by_id(params[:id])
+    else
+      redirect_to root_path
+      flash[:danger] = t("user_notfound", id: params[:id])
+    end
+  end
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = t("please_login")
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    @user = User.find_by_id(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
+
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
+end
